8549f554271f3139e7d827597c950c04941c2c7c
Rakesh Singh <rrrbisht91@gmail.com>
2024-06-18 14:09:59 +0530

    Create mockSchema.js

---
+++ models/mockSchema.js
@@ -0,0 +1,164 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Header = require('./header')
+const QParam = require('./qparam')
+const RequestBody = require('./requestBody')
+const ResponseHeader = require('./responseHeader')
+const CallBack = require('./callBack')
+const DynamicResponse = require('./dynamicResponse')
+const DynamicRequestCallback = require('./dynamicRequestCallback')
+const DynamicRequestCallbackQparams = require('./dynamicRequestCallbackQparams')
+
+
+
+const mockSchema = new Schema({
+    endPoint: String,
+    statusCodes: String,
+    contentType: String,
+    delay: String,
+    httpMethod:String,
+    body: String,
+    headers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Header'
+        }
+    ],
+    qparams:[
+
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'QParam'
+
+        }
+    ],
+    requestBody:[
+       {
+           type: Schema.Types.ObjectId,
+           ref: 'RequestBody' 
+       }
+
+    ],
+    responseHeaders:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'ResponseHeader' 
+        }
+
+    ],
+    dynamicResponse:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'DynamicResponse' 
+        }
+
+    ],
+    dynamicRequestCallback:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'DynamicRequestCallback' 
+        }
+    ],
+    dynamicRequestCallbackQparams:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'DynamicRequestCallbackQparams' 
+        }
+
+    ],
+    module:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Module' 
+        }
+
+    ],
+    callBack :[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'CallBack'   
+        }
+    ]
+});
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await DynamicRequestCallbackQparams.deleteMany({
+            _id: {
+                $in: doc.dynamicRequestCallbackQparams
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await DynamicRequestCallback.deleteMany({
+            _id: {
+                $in: doc.dynamicRequestCallback
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await DynamicResponse.deleteMany({
+            _id: {
+                $in: doc.dynamicResponse
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await CallBack.deleteMany({
+            _id: {
+                $in: doc.callBack
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Header.deleteMany({
+            _id: {
+                $in: doc.headers
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await QParam.deleteMany({
+            _id: {
+                $in: doc.qparams
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await RequestBody.deleteMany({
+            _id: {
+                $in: doc.requestBody
+            }
+        })
+    }
+})
+
+mockSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await ResponseHeader.deleteMany({
+            _id: {
+                $in: doc.responseHeaders
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('mocks', mockSchema);
