058f072287b8f85aeea87fe21e364d326dd85790
Rakesh Singh <rrrbisht91@gmail.com>
2024-06-18 14:07:22 +0530

    Create callback.js

---
+++ models/callback.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const callBackSchema = new Schema({
+    protocol: String,
+    host: String,
+    port: String,
+    path: String,
+    headers: String,
+    qparams: String,
+    delay: String,
+    httpMethod:String,
+    requestBody: String
+});
+
+module.exports = mongoose.model("CallBack", callBackSchema);
