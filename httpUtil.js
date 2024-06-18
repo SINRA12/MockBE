b69ff0edb735ee5a7ea7d8c4d4868a96754e01b1
Rakesh Singh <rrrbisht91@gmail.com>
2024-06-18 13:59:48 +0530

    Create httpUtil.js

---
+++ httpUtil.js
@@ -0,0 +1,32 @@
+module.exports =
+{
+     post: function(options,requestData){
+        const request = http.request(options, (res) => {
+            let data = '';  
+            res.on('data', (chunk) => {
+              data += chunk;
+            });
+            res.on('close', () => {
+              if(JSON.stringify(options.headers).includes('xml')){
+                console.log(res.statusCode);
+                console.log(data);              
+              }else{
+                console.log(res.statusCode);
+                console.log((data));
+              } 
+            });
+          });
+          
+          if(JSON.stringify(options.headers).includes('xml')){
+            request.write(requestData);
+          }else{
+            request.write(JSON.stringify(requestData));
+          } 
+          console.log(request);
+          request.end();
+          request.on('error', (err) => {
+           console.error(`Encountered an error trying to make a request: ${err.message}`);
+         });
+        return;
+     }
+}
